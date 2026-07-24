d01d672e275a87aeaa58c9d8c052bcd3d841eb3e
hitesh-ctrl <71893561+hitesh-ctrl@users.noreply.github.com>
2026-07-24 13:17:31 +0530

    Time: 55 ms (80.00%) | Memory: 21.9 MB (19.63%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curr_sum = 0
+        max_sum = float('-inf')
+
+        for i in nums:
+            curr_sum+=i
+            max_sum=max(max_sum,curr_sum)
+
+            if curr_sum<0:
+                curr_sum=0
+        return max_sum
+        

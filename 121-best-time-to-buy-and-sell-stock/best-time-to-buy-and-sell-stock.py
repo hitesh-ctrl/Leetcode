2e82b6c29ddf223f0f5280172cef05a5f8e3a6e5
hitesh-ctrl <71893561+hitesh-ctrl@users.noreply.github.com>
2026-08-18 21:43:14 +0530

    Time: 25 ms (97.22%) | Memory: 19 MB (74.90%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        minPrice = float('inf')
+        maxProfit=0
+        for price in prices:
+            if price<minPrice:
+                minPrice = price
+            elif price-minPrice > maxProfit:
+                maxProfit = price-minPrice
+        return maxProfit
+                
